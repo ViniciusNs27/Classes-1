ebbf9b5def1540622db29e43b320c8f9973fa1c6
ViniciusNs27 <viniciusnascimentodossantos27@gmail.com>
2026-08-08 21:35:21 -0300

    Add files via upload

---
+++ Cachorro.java
@@ -0,0 +1,16 @@
+ class Cachorro {
+
+    String nome;
+    int idade;
+    String raca;
+    String corPelo;
+    String acao;
+
+
+    public void apresentacaoCachorro(){
+        System.out.println("nome do cachorro: " + nome);
+    }
+    public void maisGostaDeFazer(){
+        System.out.println("o que o meu cachorro mais gosta de fazer é: " + acao);
+    }
+}
